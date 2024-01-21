5dd61c94eb6e8e52bc3ccb2cad27c16b7d800465
Krish Gupta <100077170+Krish-Gupta-13@users.noreply.github.com>
2024-01-21 12:42:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component{
+  state = {advice: ''};
+  componentDidMount(){
+    this.fetchadvice();
+    console.log('sfdf')
+  }
+
+  fetchadvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response)=>{
+      const {advice} = response.data.slip;
+      console.log(advice);
+      this.setState({advice: advice})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    const {advice} = this.state;
+    return(  
+      <>      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={this.fetchadvice}>
+            <span>GIVE ME A ADVICE</span>
+          </button>
+        </div>
+      </div>
+      </>
+    );
+  }
+}
+
+export default App;
